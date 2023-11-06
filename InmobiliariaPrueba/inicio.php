4e1b00c78134df549eb48d9d7356e7fc17dd12d7
CEP-AULA7-PC13\TECH <juangomr@gmail.com>
2023-11-06 15:08:01 +0100

    06/11

--- InmobiliariaPrueba/inicio.php
+++ InmobiliariaPrueba/inicio.php
@@ -7,20 +7,20 @@
   <title>Inicio Inmobiliaria</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-  <link href="../css/inicio.css" rel="stylesheet">
+  <link href="css/inicio.css" rel="stylesheet">
 </head>
 
 <body>
 
   <div class="card">
-    <img src="../imagenes/logo.jpg" class="card-img-top" alt="logotipo" />
+    <img src="imagenes/logo.jpg" class="card-img-top" alt="logotipo" />
     <div class="card-body">
       <h1 class="card-title">Inicio</h1>
       <p class="card-text">Opciones de navegación</p>
       <div class="d-grid gap-2">
-        <button onclick="window.location.href='../insertar.php'" class="btn btn-outline-dark btn-lg">Insertar
+        <button onclick="window.location.href='insertar.php'" class="btn btn-outline-dark btn-lg">Insertar
           Vivienda</button>
-        <button onclick="window.location.href='../bbdd.php'" class="btn btn-outline-dark btn-lg">Consultar
+        <button onclick="window.location.href='bbdd.php'" class="btn btn-outline-dark btn-lg">Consultar
           Vivienda</button>
         <button class="btn btn-outline-dark btn-lg">Eliminar Vivienda</button>
       </div>
